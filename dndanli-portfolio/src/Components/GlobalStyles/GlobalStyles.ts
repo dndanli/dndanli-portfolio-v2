068bdee963a82ab5c080d50db9fe3726cbd68217
Daniel De Lima <danielkcode@gmail.com>
2022-03-08 22:28:47 -0600

    removed arrow down style from global styles

--- dndanli-portfolio/src/Components/GlobalStyles/GlobalStyles.ts
+++ dndanli-portfolio/src/Components/GlobalStyles/GlobalStyles.ts
@@ -7,22 +7,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0 30rem 0 30rem;
     background-color: rgb(250, 250, 250, 0.5);
   }
-  .arrow-down-div{
-    display: flex;
-    justify-content: center;
-    margin-top: 10rem;
-    margin-bottom: 10rem;
-  }
-
-  .arrow-down-icon{
-    width: 65px;
-    height: 65px;
-    color:rgba(23,23,23, 0.5);
-    &:hover{
-      color:rgba(23,23,23, 0.8);
-      transition : 0.5s all ease-in-out;
-    }
-  }
+  
 
 `;
 
