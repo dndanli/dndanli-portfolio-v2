1ee67749196272d162ea05fc636287fcec48cf9b
Daniel De Lima <danielkcode@gmail.com>
2022-03-10 22:58:54 -0600

    removed padding ang bg color

--- dndanli-portfolio/src/Components/GlobalStyles/GlobalStyles.ts
+++ dndanli-portfolio/src/Components/GlobalStyles/GlobalStyles.ts
@@ -1,11 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body{
+  *{
     margin: 0;
     padding: 0;   
-    padding: 0 30rem 0 30rem;
-    background-color: rgb(250, 250, 250, 0.5);
+  }
+  body{
+    //padding: 0 30rem 0 30rem;
   }
 `;
 
