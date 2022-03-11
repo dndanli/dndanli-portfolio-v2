4e2057661777d66293387eb068ddeff7efaf41f3
Daniel De Lima <danielkcode@gmail.com>
2022-03-10 23:06:29 -0600

    added navbar and skills components to app.tsx

--- dndanli-portfolio/src/App.tsx
+++ dndanli-portfolio/src/App.tsx
@@ -2,14 +2,18 @@ import StyledAbout from "./Components/About/About.style";
 import GlobalStyle from "./Components/GlobalStyles/GlobalStyles";
 import StyledProjects from "./Components/Projects/Projects.style";
 import StyledArrowDown from "./Components/ArrowDownContainer/ArrowDown.style";
+import StyledNavbar from "./Components/Navbar/Navbar.style";
+import StyledSkills from "./Components/Skills/Skills.style";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <div>
+        <StyledNavbar className="nav" />
         <StyledAbout className="about-section" />
         <StyledArrowDown className="arrow-down" />
+        <StyledSkills className="skills-section" />
         <StyledProjects className="projects" />
       </div>
     </div>
