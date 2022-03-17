129630d40483f04b5eaca38eb342342adc6c1b0c
Daniel De Lima <danielkcode@gmail.com>
2022-03-16 21:58:22 -0500

    added Button component style

---
+++ dndanli-portfolio/src/Components/Button/Button.style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import LinkButton from "./Button";
+
+const StyledLinkButton = styled(LinkButton)`
+  width: 10rem;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+`;
+
+export default StyledLinkButton;
