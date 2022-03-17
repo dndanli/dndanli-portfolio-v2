42eb1bdfd9b8927f218028f8a8fda14cb05e0cf5
Daniel De Lima <danielkcode@gmail.com>
2022-03-17 18:35:38 -0500

    arrow component style update

--- dndanli-portfolio/src/Components/ArrowDownContainer/ArrowDown.style.ts
+++ dndanli-portfolio/src/Components/ArrowDownContainer/ArrowDown.style.ts
@@ -3,16 +3,18 @@ import ArrowDown from "./ArrowDown";
 const StyledArrowDown = styled(ArrowDown)`
   display: flex;
   justify-content: center;
-  margin-top: 12rem;
-  margin-bottom: 8rem;
+  padding-top: 2rem;
+  padding-bottom: 3rem;
   cursor: pointer;
+  background-color: #fdfdfd;
+  background-image: linear-gradient(45deg, #fdfdfd, #fff4f9);
 
   .arrow-down-icon {
     width: 65px;
     height: 65px;
-    color: rgba(16, 16, 16, 0.6);
+    color: rgba(16, 16, 16, 0.4);
     &:hover {
-      color: #ffc295;
+      color: #eb89b5;
       transition: 0.5s all ease-in-out;
     }
   }
