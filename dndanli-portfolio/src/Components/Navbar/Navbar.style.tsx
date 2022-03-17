c203eb3ad5911262aa95719e1248fc51c5ec2a0e
Daniel De Lima <danielkcode@gmail.com>
2022-03-17 18:39:56 -0500

    Navbar component style update

--- dndanli-portfolio/src/Components/Navbar/Navbar.style.tsx
+++ dndanli-portfolio/src/Components/Navbar/Navbar.style.tsx
@@ -2,11 +2,19 @@ import styled from "styled-components";
 import Navbar from "./Navbar";
 
 const StyledNavbar = styled(Navbar)`
-  font-family: "Montserrat", sans-serif;
-  padding: 0 30rem 0 30rem;
-  color: rgba(16, 16, 16, 0.8);
+  font-family: "Inter", sans-serif;
+  font-weight: 400;
+
+  background-image: linear-gradient(45deg, #fdfdfd, #fff4f9);
+  background-color: rgba(255, 255, 255, 0.8);
+
+  color: rgba(16, 16, 16, 0.7);
+
   height: 4rem;
+  position: fixed;
+  width: 100vw;
   ul {
+    padding: 0 30rem 0 30rem;
     list-style-type: none;
     height: 100%;
     display: flex;
@@ -18,7 +26,7 @@ const StyledNavbar = styled(Navbar)`
     cursor: pointer;
     &:hover {
       transition: all ease-in-out 350ms;
-      color: #ffc295;
+      color: rgba(16, 16, 16, 0.7);
     }
   }
 `;
