b60cd627ecfc10a43549cc6c5066ae022ba42f94
Daniel De Lima <danielkcode@gmail.com>
2022-03-17 18:48:24 -0500

    skill component page style update

--- dndanli-portfolio/src/Components/Skills/Skills.style.ts
+++ dndanli-portfolio/src/Components/Skills/Skills.style.ts
@@ -1,23 +1,21 @@
 import styled from "styled-components";
 import Skills from "../Skills/Skills";
-import BgSkills from "../../Assets/background-skills.svg";
 
 const StyledSkills = styled(Skills)`
-  font-family: "Montserrat", sans-serif;
+  font-family: "Inter", sans-serif;
+  font-weight: 400;
+
   #skills-header {
     font-size: 2.4rem;
-    margin: 10rem 30rem 3rem 30rem;
-    color: rgba(16, 16, 16, 0.8);
+    padding: 2rem 30rem 1rem 30rem;
+    color: rgba(16, 16, 16, 0.9);
   }
   .skillset-wrapper {
     display: flex;
     justify-content: center;
-    padding: 2rem 30rem 5rem 30rem;
+    padding: 6rem 30rem 4rem 30rem;
     height: 77vh;
     margin-bottom: 5rem;
-    background-image: url(${BgSkills});
-    background-repeat: no-repeat;
-    background-size: cover;
   }
 
   .skillset:nth-child(1) {
@@ -28,10 +26,6 @@ const StyledSkills = styled(Skills)`
   .skillset:nth-child(3) {
     border-top-right-radius: 10px;
     border-bottom-right-radius: 10px;
-
-    .skill-info {
-      margin-bottom: 3rem;
-    }
   }
 `;
 
