ec0505bbf4f8985e6375cb9a1c5662db2ec0089e
Daniel De Lima <danielkcode@gmail.com>
2022-03-17 18:33:34 -0500

    About page update

--- dndanli-portfolio/src/Components/About/About.tsx
+++ dndanli-portfolio/src/Components/About/About.tsx
@@ -1,3 +1,5 @@
+import StyledLinkButton from "../Button/Button.style";
+
 type AboutProps = {
   className: string;
 };
@@ -6,26 +8,42 @@ const About = ({ className }: AboutProps) => {
   return (
     <div className={className}>
       <div className="greetings">
-        <div className="about-header">
+        <div>
           <h1>
-            <span id="hey">Hey!</span> I'M DANIEL DE LIMA
+            HEY! I'M
+            <span id="name"> DANIEL DE LIMA</span>
           </h1>
-          <h1>A DIGITAL CRAFTSMAN</h1>
+          <h1>
+            A <span id="about-title">DIGITAL CRAFTSMAN</span>
+          </h1>
         </div>
-        <img
-          className="profile-pic"
-          src="https://avatars.githubusercontent.com/u/73452073?v=4"
-          alt="daniel-profile-pic"
-          width={150}
-          height={150}
-        ></img>
       </div>
       <div className="about-text">
-        <h2>I'm a full-stack developer based in iowa.</h2>
-        <h2>
+        <p>
+          I'm a full-stack developer that specializes in building high quality
+          web based software.
+        </p>
+        <p>
           Since you're here why don't you check my skills and projects down
           below?
-        </h2>
+        </p>
+        <div id="buttons">
+          <StyledLinkButton
+            className="link-button"
+            textInput="LinkedIn"
+            link="https://www.linkedin.com/in/daniel-de-lima-a07aa8223/"
+            inputBgColor="black"
+            inputColor="white"
+          ></StyledLinkButton>
+
+          <StyledLinkButton
+            className="link-button"
+            textInput="GitHub"
+            link="https://github.com/dndanli"
+            inputBgColor="#d0e0f6"
+            inputColor="black"
+          ></StyledLinkButton>
+        </div>
       </div>
     </div>
   );
