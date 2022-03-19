e1a966e3939d020ce1bbc5325e3b6c428599a3df
Daniel De Lima <danielkcode@gmail.com>
2022-03-18 23:32:32 -0500

    navbar style update

--- dndanli-portfolio/src/Components/Navbar/Navbar.style.tsx
+++ dndanli-portfolio/src/Components/Navbar/Navbar.style.tsx
@@ -4,23 +4,23 @@ import Navbar from "./Navbar";
 const StyledNavbar = styled(Navbar)`
   font-family: "Inter", sans-serif;
   font-weight: 400;
-
-  background-image: linear-gradient(45deg, #fdfdfd, #fff4f9);
   background-color: rgba(255, 255, 255, 0.8);
-
+  background-image: linear-gradient(45deg, #fdfdfd, #fff4f9);
   color: rgba(16, 16, 16, 0.5);
+  padding-inline: min(30vw, 25rem);
 
-  height: 4rem;
   position: fixed;
-  width: 100vw;
+  min-width: 100vw;
+  display: flex;
+  justify-content: center;
+  height: 4rem;
+
   ul {
-    padding: 0 30rem 0 30rem;
     list-style-type: none;
-    height: 100%;
     display: flex;
-    justify-content: flex-end;
     align-items: center;
   }
+
   li {
     font-size: 1.2rem;
     margin-left: 2rem;
