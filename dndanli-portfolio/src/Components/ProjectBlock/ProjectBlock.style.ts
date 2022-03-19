b95dcf619d6f57207167afaff6694af3577e4d75
Daniel De Lima <danielkcode@gmail.com>
2022-03-18 23:44:35 -0500

    Project block style update

--- dndanli-portfolio/src/Components/ProjectBlock/ProjectBlock.style.ts
+++ dndanli-portfolio/src/Components/ProjectBlock/ProjectBlock.style.ts
@@ -2,21 +2,10 @@ import styled from "styled-components";
 import ProjectBlock from "./ProjectBlock";
 
 const StyledProjectBlock = styled(ProjectBlock)`
-  margin-bottom: 5rem;
   display: flex;
-  width: 49vw;
-  padding: 0.8rem;
-
-  .project-img {
-    width: 500px;
-    height: 240px;
-    margin-top: 1.4rem;
-  }
-
-  .project-img:nth-child(odd) {
-    margin-right: 1.5rem;
-  }
-
+  justify-content: center;
+  align-items: center;
+  width: 60rem;
   .project-desc,
   .project-title,
   .project-stack {
@@ -25,10 +14,19 @@ const StyledProjectBlock = styled(ProjectBlock)`
   }
 
   .project-desc {
-    width: 500px;
-    margin-right: 4rem;
+    padding: 1rem 2.5rem 1rem 1rem;
   }
+  .project-img {
+    width: 400px;
+    height: 300px;
+    border-radius: 8px;
+    box-shadow: 0 0 15px rgba(16, 16, 16, 0.2);
 
+    &:hover {
+      box-shadow: 0 0 15px rgba(16, 16, 16, 0.4);
+      transition: all ease-in-out 0.5s;
+    }
+  }
   .project-title {
     font-size: 1.5rem;
     margin-bottom: 1rem;
