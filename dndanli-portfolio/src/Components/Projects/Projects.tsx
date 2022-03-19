c15d94ff89d472a6d0d02c29fd9741e372094273
Daniel De Lima <danielkcode@gmail.com>
2022-03-18 23:40:31 -0500

    added wrapper

--- dndanli-portfolio/src/Components/Projects/Projects.tsx
+++ dndanli-portfolio/src/Components/Projects/Projects.tsx
@@ -9,18 +9,20 @@ const Projects = ({ className }: ProjectsProp) => {
   return (
     <div className={className} id="all-projects">
       <h2 id="projects-title">Projects</h2>
-      {ProjectsData.map((project) => {
-        return (
-          <StyledProjectBlock
-            key={project.id}
-            className="project-block"
-            projectImgUrl={project.imgUrl}
-            projectTitle={project.title}
-            projectDesc={project.desc}
-            projectStack={project.stack}
-          />
-        );
-      })}
+      <div className="projects-wrapper">
+        {ProjectsData.map((project) => {
+          return (
+            <StyledProjectBlock
+              key={project.id}
+              className="project-block"
+              projectImgUrl={project.imgUrl}
+              projectTitle={project.title}
+              projectDesc={project.desc}
+              projectStack={project.stack}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
