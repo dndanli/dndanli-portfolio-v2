a2e4f23724409f865095be0c6bd2f0f9e1e15cb4
Daniel De Lima <danielkcode@gmail.com>
2022-03-20 14:37:13 -0500

    added recent works styling

--- dndanli-portfolio/src/Components/Sections/Projects/Projects.style.ts
+++ dndanli-portfolio/src/Components/Sections/Projects/Projects.style.ts
@@ -8,6 +8,11 @@ const StyledProjects = styled(Projects)`
   .project-card {
     margin-bottom: 10%;
   }
+  #recent-works {
+    font-family: "Inter", sans-serif;
+    color: rgba(250, 250, 250, 0.8);
+    margin-bottom: 1rem;
+  }
 `;
 
 export default StyledProjects;
