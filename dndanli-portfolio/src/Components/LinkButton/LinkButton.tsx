209c49a6c454ac7f55cad6a00902cf1b5712a68b
Daniel De Lima <danielkcode@gmail.com>
2022-03-20 13:58:47 -0500

    renamed button to link button

---
+++ dndanli-portfolio/src/Components/LinkButton/LinkButton.tsx
@@ -0,0 +1,27 @@
+type LinkButtonProps = {
+  className: string;
+  textInput: string;
+  link: string;
+  inputColor: string;
+  inputBgColor: string;
+};
+const LinkButton = ({
+  className,
+  textInput,
+  link,
+  inputColor,
+  inputBgColor,
+}: LinkButtonProps) => {
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <div
+        className={className}
+        style={{ backgroundColor: inputBgColor, color: inputColor }}
+      >
+        <h2 className="btn-text-input">{textInput}</h2>
+      </div>
+    </a>
+  );
+};
+
+export default LinkButton;
