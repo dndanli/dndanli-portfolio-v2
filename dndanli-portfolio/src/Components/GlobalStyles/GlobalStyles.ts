ceb8c8226d01bff437cda0b2c5285bb3e2a452a1
Daniel De Lima <danielkcode@gmail.com>
2022-03-20 18:19:36 -0500

    global styles update

--- dndanli-portfolio/src/Components/GlobalStyles/GlobalStyles.ts
+++ dndanli-portfolio/src/Components/GlobalStyles/GlobalStyles.ts
@@ -4,18 +4,17 @@ const GlobalStyle = createGlobalStyle`
   *{
     margin: 0;
     padding: 0;   
+    box-sizing: border-box;
   }
+  body {  
 
-  body{
-    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     padding-inline: min(80vw, 10rem);
     background-color: #48284A;
     background-image: linear-gradient(45deg, #121417, #48284A);
-
-    @media only screen and (max-width: 600px){
-        padding-inline: min(80vw, 2rem);
-
-    }
   }
 
 
