501159aa94e2d0003204530b08d9e95a4a5a1ef1
Daniel De Lima <danielkcode@gmail.com>
2022-03-20 14:08:42 -0500

    added components to app.tsx

--- dndanli-portfolio/src/App.tsx
+++ dndanli-portfolio/src/App.tsx
@@ -2,16 +2,16 @@ import StyledAbout from "./Components/Sections/About/About.style";
 import GlobalStyle from "./Components/GlobalStyles/GlobalStyles";
 import StyledNavbar from "./Components/Navbar/Navbar.style";
 import StyledSkills from "./Components/Sections/Skills/Skills.style";
+import StyledProjects from "./Components/Sections/Projects/Projects.style";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <div>
-        <StyledNavbar className="nav" />
-        <StyledAbout className="about-section" />
-        <StyledSkills className="skills-section" />
-      </div>
+      <StyledNavbar className="nav" />
+      <StyledAbout className="about-section" />
+      <StyledSkills className="skills-section" />
+      <StyledProjects className="projects-section" />
     </div>
   );
 }
