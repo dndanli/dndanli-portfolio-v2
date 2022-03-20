4c53e8795e5c14ae2bbc41ca4c5054cc60824468
Daniel De Lima <danielkcode@gmail.com>
2022-03-19 21:08:24 -0500

    moved Skills Data to Data folder

---
+++ dndanli-portfolio/src/Data/SkillsData.ts
@@ -0,0 +1,31 @@
+const SkillsData: {
+  id: number;
+  title: string;
+  skills: string[];
+}[] = [
+  {
+    id: 1,
+    title: "Frontend",
+    skills: ["JavaScript", "TypeScript", "React", "HTML", "CSS", "Bootstrap"],
+  },
+  {
+    id: 2,
+    title: "Backend",
+    skills: [
+      "Java",
+      "SpringBoot",
+      "NodeJs",
+      "Express",
+      "Python",
+      "Django",
+      "SQL",
+    ],
+  },
+  {
+    id: 3,
+    title: "Tools",
+    skills: ["Git", "GitHub", "Heroku", "VsCode", "NeoVim", "Windows", "Linux"],
+  },
+];
+
+export { SkillsData };
