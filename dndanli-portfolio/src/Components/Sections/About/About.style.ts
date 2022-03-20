de52a0f11dd1d0f46abe18923dce5a3a42afdaaa
Daniel De Lima <danielkcode@gmail.com>
2022-03-19 21:10:49 -0500

    moved about section component style to section folder

---
+++ dndanli-portfolio/src/Components/Sections/About/About.style.ts
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+import About from "./About";
+
+const StyledAbout = styled(About)`
+  display: flex;
+  flex-direction: column;
+  min-height: 40rem;
+  padding-block: min(30vh, 5rem);
+
+  .greetings {
+    font-size: 1.3rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 700;
+    color: rgba(250, 250, 250, 0.9);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #span-greeting-1 {
+    /* color: #fc85ae; */
+  }
+
+  #span-gretting-2 {
+    /* color: #f4afab; */
+  }
+
+  .about-text {
+    font-size: 1.2rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 400;
+
+    margin-top: 2rem;
+    padding-inline: min(30vw, 2rem);
+    color: rgba(250, 250, 250, 0.9);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .about-text p {
+    margin-bottom: 1rem;
+  }
+
+  .buttons-wrapper {
+    justify-content: center;
+    margin-top: 2rem;
+    display: flex;
+  }
+
+  .buttons-wrapper a {
+    font-size: 15px;
+    text-decoration: none;
+  }
+
+  .buttons-wrapper a:nth-child(odd) {
+    margin-left: auto;
+    margin-right: 2.5%;
+  }
+
+  .buttons-wrapper a:nth-child(even) {
+    margin-left: 2.5%;
+    margin-right: auto;
+  }
+
+  .link-button {
+    width: 7rem;
+  }
+
+  .arrow-down-icon {
+    margin-top: 20%;
+    font-size: 3rem;
+    display: flex;
+    justify-content: center;
+    color: rgba(250, 250, 250, 0.7);
+  }
+`;
+
+export default StyledAbout;
