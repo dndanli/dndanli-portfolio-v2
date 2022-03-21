51a1d84dd68ac7a2681bf6c081014b74f4ab0cd6
Daniel De Lima <danielkcode@gmail.com>
2022-03-20 21:59:22 -0500

    Global styles update

--- dndanli-portfolio/src/Components/GlobalStyles/GlobalStyles.ts
+++ dndanli-portfolio/src/Components/GlobalStyles/GlobalStyles.ts
@@ -11,17 +11,53 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     justify-content: center;
     align-items: center;
-
-    padding-inline: min(80vw, 10rem);
+    padding-inline: min(80vw, 3rem);
     background-color: #48284A;
     background-image: linear-gradient(45deg, #121417, #48284A);
   }
 
+  /* 320 - 480 mobile devices */
+  .about-section{
+    @media only screen and (min-width: 480px){
+      .greetings{
+        font-size: 1.2rem;
+      }
+      .about-text{
+        font-size: 1.2rem;
+      }
+    }
+  }
 
- 
+  /* 481 - 678 Ipads tablets*/
+  .skills-section{
+   
+  }
 
+  .about-section{
+    @media only screen and (min-width: 678px){
+      .greetings{
+          font-size: 1.3rem;
+        }
+    }
+  }
 
-  
+  .skills-section{
+    @media only screen and (min-width: 769px){
+      .skills-container{
+        display: grid;
+        justify-content: center;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 3rem;
+      }
+      #tools{
+        grid-column: 1/3;
+        .skill-list-item{
+          margin-left: 7rem;
+        }
+        
+      }    
+    }
+  }  
 `;
 
 export default GlobalStyle;
