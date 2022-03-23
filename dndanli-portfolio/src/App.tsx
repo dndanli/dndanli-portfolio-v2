179882aff374f1c487e8892b063b5dac6cf4eed7
Daniel De Lima <danielkcode@gmail.com>
2022-03-23 15:16:52 -0500

    imported and added contact section component

--- dndanli-portfolio/src/App.tsx
+++ dndanli-portfolio/src/App.tsx
@@ -3,6 +3,7 @@ import GlobalStyle from "./Components/GlobalStyles/GlobalStyles";
 import StyledNavbar from "./Components/Navbar/Navbar.style";
 import StyledSkills from "./Components/Sections/Skills/Skills.style";
 import StyledProjects from "./Components/Sections/Projects/Projects.style";
+import StyledContact from "./Components/Sections/Contact/Contact.style";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <StyledAbout className="about-section" />
       <StyledSkills className="skills-section" />
       <StyledProjects className="projects-section" />
+      <StyledContact className="contact-section" />
     </div>
   );
 }
