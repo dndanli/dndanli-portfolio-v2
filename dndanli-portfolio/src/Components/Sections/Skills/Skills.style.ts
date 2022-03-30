084f47aecd4da190fd358bb9c8a34d4dcd9db4ed
Daniel De Lima <danielkcode@gmail.com>
2022-03-30 02:29:58 -0500

    skill cards color update

--- dndanli-portfolio/src/Components/Sections/Skills/Skills.style.ts
+++ dndanli-portfolio/src/Components/Sections/Skills/Skills.style.ts
@@ -10,12 +10,28 @@ const StyledSkills = styled(Skills)`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  #frontend > .skillset-card {
+    background-color: #f6c4ed;
+    background-image: linear-gradient(315deg, #e1dae6 0%, #f6c4ed 74%);
+  }
+
+  #backend > .skillset-card {
+    background-color: #ffcaa6;
+    background-image: linear-gradient(315deg, #f9d1b7 0%, #ffcaa6 74%);
+  }
+
+  #tools > .skillset-card {
+    background-color: #f2ddc1;
+    background-image: linear-gradient(315deg, #f2ddc1 0%, #fbe9d7 74%);
+  }
+
   .skillset-card {
     margin-bottom: 10%;
   }
 
   #skill-header-title {
-    color: rgba(57, 62, 70, 0.9);
+    color: rgba(16, 16, 16, 0.8);
     margin-bottom: 1rem;
   }
 `;
